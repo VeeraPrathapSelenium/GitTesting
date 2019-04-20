352bd9f909ba01d2bb3937b4e3a3e64afb40fac2
tm <tm@cl1>
2019-04-20 12:17:30 +0530

    first commit

---
+++ src/test/java/com/ReflectionTest/GenericMethods.java
@@ -0,0 +1,22 @@
+package com.ReflectionTest;
+
+public class GenericMethods {
+	
+	static {
+		
+		System.out.println("Class is loading");
+	}
+	
+	public void login()
+	{
+		System.out.println("I am logging into system");
+	}
+	
+	
+	public void register()
+	{
+		System.out.println("Registration page");
+	}
+	
+
+}
---
+++ src/test/java/com/ReflectionTest/TestReflection.java
@@ -0,0 +1,20 @@
+package com.ReflectionTest;
+
+import org.junit.Test;
+
+public class TestReflection {
+
+	
+@Test
+
+public  void Test()
+{
+	
+	System.out.println("Do testing");
+}
+	
+	
+	
+	
+
+}
